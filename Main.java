4c7934d67ec663521de4c70b87da46c9c4c4836a
Diego Hernandez <124943402+dhp001@users.noreply.github.com>
2023-03-23 17:25:04 +0000

    Fixed variable names and typos.

--- Main.java
+++ Main.java
@@ -20,17 +20,15 @@ class Inventory {
 class Main {
 	public static void main(String[] args) {
     String name, serialID;
-    int price, choice;
+    int price, option;
     Scanner input = new Scanner(System.in);
     
     ArrayList<Inventory> catalog = new ArrayList<Inventory>();
     
     do{
       System.out.printf("Press 1 to add an item.\nPress 2 to delete an item.\nPress 3 to update an item.\nPress 4 to show all the items.\nPress 5 to quit the program.\n");
-      choice=input.nextInt();
-
-      
-      switch(choice){
+      option=input.nextInt();
+      switch(option){
         case 1:
         System.out.println("Enter the name:");
         name=input.nextLine();
@@ -41,12 +39,14 @@ class Main {
           
         System.out.println("Enter the value in dollars (whole number):");
         price=input.nextInt();
+        input.nextLine();
         catalog.add(new Inventory(name, serialID, price));
         break;
 
         case 2:
-        System.out.println("Enter the serial number of the intem to delete:");
         String delID=input.nextLine();
+        System.out.println("Enter the serial number of the item to delete:");
+        delID=input.nextLine();
         for (int i = 0; i < catalog.size(); i++){
           if (catalog.get(i).serialID.equals(delID)) {
             catalog.remove(i);
@@ -56,18 +56,18 @@ class Main {
 
         case 3:
         System.out.println("Enter the serial number of the item to change:");
-      String changeID = scanner.nextLine();
+      String changeID = input.nextLine();
           
         for (int i = 0; i < catalog.size(); i++){
           if (catalog.get(i).serialID.equals(changeID)){
             System.out.println("Enter the new name:");
-            String newName = scanner.nextLine();
+            String newName = input.nextLine();
             
             catalog.get(i).name = newName;
-            System.out.println("Enter the new value in dollars(whole number):");
-            int newValue = scanner.nextInt();
-            scanner.nextLine();
-            catalog.get(i).value = newValue;
+            System.out.println("Enter the new value in dollars (whole number):");
+            int newValue = input.nextInt();
+            input.nextLine();
+            catalog.get(i).price = newValue;
           }
         }
         
@@ -79,6 +79,6 @@ class Main {
         }
         break;
       }
-      }while(choice!=5);
+      }while(option!=5);
 }
 }
